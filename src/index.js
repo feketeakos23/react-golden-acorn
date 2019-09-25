7661aaa553880f7aa614c5ec83635e3057616d88
Ákos Fekete <feketeakos23@users.noreply.github.com>
2019-09-25 15:41:45 +0200

    Structure created

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'button';
+import { Display } from 'display';
+
+class SimpleGoldenAcornApp extends React.Component {
+    render() {
+        return ()
+    }
+}
+
+const mountNode = document.getElementById('root');
+ReactDOM.render(<SimpleGoldenAcornApp />, mountNode);
